8d73e6a7763a788888b3f080f1109158cbca7a33
harshvs99 <harshvs1999@gmail.com>
2022-09-28 03:20:31 +0530

    Make edits for managing file sync-ups

--- download_music.py
+++ download_music.py
@@ -11,15 +11,18 @@ download_links = json.load(dlinks)
 def download_file_tidal(album):
     url = download_links[album]
     r_payload = requests.get(url)
-    print("the payload: ", r_payload)
     file_name = album.replace(" ", "_")
     open(music_dl_folder+file_name+".zip", 'wb').write(r_payload.content)
     print(file_name, " is downloaded")
     with zipfile.ZipFile(music_dl_folder+file_name+".zip", 'r') as zip_ref:
         zip_ref.extractall(music_dl_folder)
     print("Files downloaded to: ", music_dl_folder)
-
+ 
 all_albums = ["2014 Forest Hills Drive"]
 for album in all_albums:
     download_file_tidal(album)
+
+os.system("rm -rf music_dl/*.zip")
 os.system(f"aws s3 sync {music_dl_folder} s3://harshvs-stream-music")
+os.system("rm -rf music_dl/*")
+
